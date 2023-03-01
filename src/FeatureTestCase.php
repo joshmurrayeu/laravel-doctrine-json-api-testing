8ae22cd29171f5d9097d378dd82b821b9abe307d
Josh Murray <josh.murray@simpleonlinehealthcare.com>
2023-03-01 10:44:12 +0000

    Add the base classes

---
+++ src/FeatureTestCase.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests;
+
+use Doctrine\ORM\EntityManager;
+
+abstract class FeatureTestCase extends TestCase
+{
+    /**
+     * FeatureTestCase set-up.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        /** @var EntityManager $entityManager */
+        $entityManager = app(EntityManager::class);
+
+        $schemaManager = $entityManager->getConnection()->getSchemaManager();
+        $tables = $schemaManager->listTables();
+
+        if (count($tables) > 1) {
+            // unlink(env('DB_DATABASE'));
+        }
+
+        if (count($tables) === 0) {
+            $this->artisan('doctrine:schema:create --no-interaction');
+        }
+    }
+}

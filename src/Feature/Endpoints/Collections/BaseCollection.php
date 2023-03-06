f4ebd77951c340e9a0a9959d80a076ec48a4c23a
Josh Murray <josh.murray@simpleonlinehealthcare.com>
2023-03-06 15:55:35 +0000

    Updated a helper function within the Collection namespace

--- src/Feature/Endpoints/Collections/BaseCollection.php
+++ src/Feature/Endpoints/Collections/BaseCollection.php
@@ -18,16 +18,16 @@ abstract class BaseCollection
     }
 
     /**
-     * @param int|null $id
+     * @param mixed ...$params
      *
      * @return string
      */
-    public function url(?int $id = null): string
+    public function url(mixed ...$params): string
     {
         $url = "{$this->getVersion()}/{$this->getResourceType()}";
 
-        if (!empty($id)) {
-            $url .= "/{$id}";
+        if (!empty($params)) {
+            $url .= '/' . implode('/', $params);
         }
 
         return url($url);
